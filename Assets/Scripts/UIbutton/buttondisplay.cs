864b8854a9f935d47c587f09197042b67e4393e4
chaos0930 <1840088217@qq.com>
2023-06-09 11:26:30 +0800

    合并分支hjl

---
+++ Assets/Scripts/UIbutton/buttondisplay.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class buttondisplay : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
+{
+    public Color normalColor = new(1f, 1f, 1f,0.5f);
+    public Color highLightColor = new(0f, 0f, 0f, 0.5f);
+
+    public Image image;
+
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.color = normalColor;
+        
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        image.color = highLightColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.color = normalColor;
+    }
+}
